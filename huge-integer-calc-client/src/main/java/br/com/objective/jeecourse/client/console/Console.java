4fb266b4df593422d471c7a3dcd8cb1a2b7be759
U-OBJECTIVEMGA\luiz.taborda <luiz.taborda@WSMGA-TABORDA.(none)>
2011-06-06 20:30:55 -0300

    Sub-Modulo Spykes

--- huge-integer-calc-client/src/main/java/br/com/objective/jeecourse/client/console/Console.java
+++ huge-integer-calc-client/src/main/java/br/com/objective/jeecourse/client/console/Console.java
@@ -2,11 +2,7 @@ package br.com.objective.jeecourse.client.console;
 
 public class Console {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
