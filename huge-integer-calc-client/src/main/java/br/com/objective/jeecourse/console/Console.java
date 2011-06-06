d7dc84a71a581bba74c118818d669fbe5e75e72c
Lohandus Ribeiro <lohandus@gmail.com>
2011-06-06 13:09:45 -0300

    Pacotes console e gui do client

---
+++ huge-integer-calc-client/src/main/java/br/com/objective/jeecourse/console/Console.java
@@ -0,0 +1,13 @@
+package br.com.objective.jeecourse.console;
+
+public class Console {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ huge-integer-calc-client/src/main/java/br/com/objective/jeecourse/gui/Gui.java
@@ -0,0 +1,13 @@
+package br.com.objective.jeecourse.gui;
+
+public class Gui {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
